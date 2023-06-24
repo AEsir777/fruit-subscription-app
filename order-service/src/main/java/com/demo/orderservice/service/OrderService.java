5f95f9e660f3015512881f34d08c2ea95fbda69f
AEsir777 <lkb2438834817@gmail.com>
2023-06-24 15:00:46 -0400

    add orderService

---
+++ order-service/src/main/java/com/demo/orderservice/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.demo.orderservice.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.demo.orderservice.dto.OrderLineItemDto;
+import com.demo.orderservice.dto.OrderRequest;
+import com.demo.orderservice.model.Order;
+import com.demo.orderservice.model.OrderLineItem;
+import com.demo.orderservice.repository.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    private OrderLineItem mapDtoToItem(OrderLineItemDto orderLineItemDto) {
+        OrderLineItem orderLineItem = new OrderLineItem();
+        orderLineItem.setPrice(orderLineItemDto.getPrice());
+        orderLineItem.setQuanity(orderLineItemDto.getQuanity());
+        orderLineItem.setSkuCode(orderLineItemDto.getSkuCode());
+        return orderLineItem;
+    }
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItem> orderLineItem = orderRequest.getOrderLineItemDtoList()
+                .stream()
+                .map(this::mapDtoToItem)
+                .toList();
+        order.setOrderLineItemList(orderLineItem);
+
+        // place the order
+        orderRepository.save(order);
+    }
+}
